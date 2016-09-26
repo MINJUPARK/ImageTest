b5f529e4e3de21c0aeaed5538cf9b396fb5cf17e
minju0227@e-mirim.hs.kr <minju0227@e-mirim.hs.kr>
2016-09-26 20:40:54 +0900

    #2 Independent My Image Class

---
+++ app/src/main/java/com/example/imagetest/MyImage.java
@@ -0,0 +1,33 @@
+package com.example.imagetest;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.view.View;
+
+/**
+ * Created by 박민주 on 2016-09-26.
+ */
+public class MyImage extends View {
+    MyImage(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+
+        super.onDraw(canvas);
+        Bitmap picture = BitmapFactory.decodeResource(getResources(),R.drawable.cat);
+        float cx = getWidth()/2.0f; // 중심점 x
+        float cy = getHeight()/2.0f; // 중심점 Y
+        float x = (getWidth()-picture.getWidth())/2.0f;
+        float y = (getHeight()-picture.getWidth())/2.0f;
+        //canvas.rotate(45,cx,cy); // 이미지 회전
+        //canvas.translate(-150,200); // 이미지 이동
+        canvas.scale(1.5f,1.5f,cx,cy); // 이미지 크기
+        canvas.skew(0.4f, 0.4f); // 이미지 비틀기
+
+        canvas.drawBitmap(picture,x,y,null);
+    }
+}
